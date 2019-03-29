39c84d1c87a793d1804fce02230b4b31ca613e96
dou000 <dousongbo@vip.com>
2019-03-29 10:00:29 +0800

    second

---
+++ spring-boot-shiro/src/main/java/com/xxx/shiro/ShiroDemoApplication.java
@@ -0,0 +1,17 @@
+package com.xxx.shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author dousongbo
+ * @Date 2019/3/29-9:30
+ */
+@SpringBootApplication
+public class ShiroDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroDemoApplication.class,args);
+    }
+}
